4c5fc5b504634e79f57c06111daaf0b1394b1e2d
Janiece Rodriguez <rodriguezjaniece@yahoo.com>
2020-04-02 15:51:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,3 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "------------"
@@ -8,24 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def turn(board)
-  puts " Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
-if valid_move?(board, index) == true
-      move(board, index, "X")
-      display_board(board)
-    else
-      turn(board)
-    end
-  end
-
 def input_to_index(user_input)
   user_input.to_i - 1
-end
-
-def move(array, index, value= "X")
-  array[index] = value
 end
 
 def valid_move?(board, index)
@@ -35,6 +17,10 @@ def valid_move?(board, index)
   end
 end
 
+def move(array, index, value= "X")
+  array[index] = value
+end
+
 def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     false
@@ -42,3 +28,15 @@ def position_taken?(board, index)
     true
  end
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
