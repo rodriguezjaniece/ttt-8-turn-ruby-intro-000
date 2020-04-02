9771c0431f565b58afd54cdadeb9e8dbdde8a319
Janiece Rodriguez <rodriguezjaniece@yahoo.com>
2020-04-02 16:04:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]}"
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
-end 
+end
 
 def input_to_index(user_input)
   user_input.to_i - 1
