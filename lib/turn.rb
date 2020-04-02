ec25307abff010b3d52546f14e1f1d58aab747ff
Janiece Rodriguez <rodriguezjaniece@yahoo.com>
2020-04-02 15:45:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,13 +12,13 @@ def turn(board)
   puts " Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-
-    if valid_move?(board, index) == true
+if valid_move?(board, index) == true
       move(board, index, "X")
       display_board(board)
     else
       turn(board)
     end
+  end
 
 def input_to_index(user_input)
   user_input.to_i - 1
@@ -32,9 +32,10 @@ def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
     true
   else false
+  end
 end
 
-def position_taken?(bpard, index)
+def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     false
   elsif (board[index] == "X" || board[index] == "O")
